fb10c50b323534eec6d3b458cd1568daa63ef407
Federico Valla <38297526+vallafederico@users.noreply.github.com>
2025-01-08 14:45:24 +0100

    button states for slider

--- dist/app.js
+++ dist/app.js
@@ -9066,9 +9066,25 @@
     constructor(el) {
       this.el = el;
       this.api = EmblaCarousel(this.el, this.options);
+      this.checkEnabled();
       this.btns.forEach((btn, i) => {
-        btn.onclick = () => i === 0 ? this.api.scrollPrev() : this.api.scrollNext();
+        btn.onclick = () => {
+          i === 0 ? this.api.scrollPrev() : this.api.scrollNext();
+          this.checkEnabled();
+        };
       });
+    }
+    checkEnabled() {
+      if (this.api.canScrollNext()) {
+        this.btns[1].classList.remove("disabled");
+      } else {
+        this.btns[1].classList.add("disabled");
+      }
+      if (this.api.canScrollPrev()) {
+        this.btns[0].classList.remove("disabled");
+      } else {
+        this.btns[0].classList.add("disabled");
+      }
     }
     destroy() {
       this.btns.forEach((btn) => btn.onclick = null);
--- src/modules/slider.js
+++ src/modules/slider.js
@@ -7,11 +7,30 @@ export class Slider {
     this.el = el;
 
     this.api = EmblaCarousel(this.el, this.options);
+    this.checkEnabled();
 
     this.btns.forEach((btn, i) => {
-      btn.onclick = () =>
+      btn.onclick = () => {
         i === 0 ? this.api.scrollPrev() : this.api.scrollNext();
+        this.checkEnabled();
+      };
     });
+  }
+
+  checkEnabled() {
+    // console.log(this.api.canScrollNext(), this.api.canScrollPrev());
+
+    if (this.api.canScrollNext()) {
+      this.btns[1].classList.remove("disabled");
+    } else {
+      this.btns[1].classList.add("disabled");
+    }
+
+    if (this.api.canScrollPrev()) {
+      this.btns[0].classList.remove("disabled");
+    } else {
+      this.btns[0].classList.add("disabled");
+    }
   }
 
   destroy() {
