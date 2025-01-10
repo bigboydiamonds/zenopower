4d54c0fb9ecf0587bb1178c32ac9ac25ffe6adcc
Federico Valla <38297526+vallafederico@users.noreply.github.com>
2025-01-10 14:01:38 +0100

    bump model

--- dist/app.js
+++ dist/app.js
@@ -17987,7 +17987,7 @@ ${addLineNumbers(fragment2)}`);
     matcap2: URL2 + "metal4.png",
     model: URL2 + "zeno3.glb",
     light: URL2 + "light1.png",
-    light2: URL2 + "light2.png"
+    light2: URL2 + "light3.png"
   };
 
   // src/gl/util/loader.js
